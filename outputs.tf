9fdc04e8f0c94ddb3dbf394dd685300e7ac5a65e
Roberth Strand <roberth.strand@crayon.com>
2021-05-19 12:42:15 +0200

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
 output "host" {
-  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].host
   description = "The Kubernetes cluster server host."
+  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].host
 }
 output "client_certificate" {
-  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_certificate
   description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_certificate
 }
 output "client_key" {
-  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_key
   description = "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_key
 }
 output "cluster_ca_certificate" {
-  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].cluster_ca_certificate
   description = "Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0].cluster_ca_certificate
 }
 output "aks_credentials" {
-  value = format("az aks get-credentials --resource-group %s --name %s", data.azurerm_resource_group.aks.name, azurerm_kubernetes_cluster.cluster.name)
+  description = "The AZ CLI command to get credentials from your new cluster."
+  value       = format("az aks get-credentials --resource-group %s --name %s", data.azurerm_resource_group.aks.name, azurerm_kubernetes_cluster.cluster.name)
+}
+output "kube_admin_config_raw" {
+  description = "The raw kube admin config, used with kubectl and other tools."
+  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config_raw
 }
