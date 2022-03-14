e09ed8157ae55795c0c988960eba029edf6b4ac4
Roberth Strand <roberth.strand@crayon.com>
2022-03-14 10:59:55 +0100

    fix(deployment-location): location is now a variable, no longer uses a data source
    using a data source made the deployment want to redeploy the entire cluster if the underlaying resource group had any minor changes to it, making it hard to have a stable deployment with the module. Removing the data source and asking for a location input shouldn't make it harder to use but removes any weird dependency issues.
    
    closes #60
    
    Signed-off-by: Roberth Strand <roberth.strand@crayon.com>

--- data.tf
+++ data.tf
@@ -1,7 +1,3 @@
-data "azurerm_resource_group" "aks" {
-  name = var.resource_group
-}
-
 data "azurerm_kubernetes_service_versions" "current" {
   location        = data.azurerm_resource_group.aks.location
   version_prefix  = var.kubernetes_version_prefix
